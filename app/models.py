f2f4757afa2af472ac1c80523bf068729a27f394
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-05-10 21:18:08 -0400

    finished user login

--- app/models.py
+++ app/models.py
@@ -4,8 +4,14 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     password: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
@@ -15,10 +21,10 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
     
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
 class Video(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,44 @@
-from flask import render_template, redirect, url_for
-from app import app
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app import app, db
 from app.forms import LoginForm
+from app.models import User
+from urllib.parse import urlsplit
 
 @app.route("/")
-def route_home():
+@login_required
+def index():
     return render_template("index.html")
 
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
 @app.route("/login", methods=['GET', 'POST'])
-def route_login():
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        print('Login from user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect("/")
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for("index")
+        return redirect(url_for("index"))
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route("/user/settings")
+@login_required
+def settings():
+    return "hi"
