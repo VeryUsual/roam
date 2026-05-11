f36015ba6041e34028638a0c306a47edc7ab4f45
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-05-11 07:04:35 -0400

    editing profiles, registering, error handling

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -16,6 +17,8 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     password: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     videos: so.WriteOnlyMapped["Video"] = so.Relationship(back_populates="author")
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -25,6 +28,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    def avatar(self, size):
+        digest = md5(self.username.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 class Video(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,16 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
 
 @app.route("/")
 @login_required
@@ -33,12 +40,44 @@ def login():
         return redirect(url_for("index"))
     return render_template("login.html", title="Login", form=form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now registered! Please log in with your details.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route("/user/settings")
+@app.route("/edit_profile", methods=['GET', 'POST'])
 @login_required
-def settings():
-    return "hi"
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/user/<username>')
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    videos = [
+        {'author': user, 'filepath': 'videos/test.mp4'}
+    ]
+    return render_template('user.html', user=user, videos=videos)
